5eaee1019c6f75638f2b8e85d035ab39bdfc00f7
Zachary Lim <zacharylim2004@gmail.com>
2022-12-14 21:07:22 +0800

    Added model helpers

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -3,29 +3,60 @@
 namespace App\Models;
 
 use App\Traits\UUID;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\Models\Property
+ *
+ * @property string $id
+ * @property string $user_id
+ * @property string $name
+ * @property string $address
+ * @property int $bedrooms
+ * @property int $bathrooms
+ * @property string $description
+ * @property string $price
+ * @property string|null $sq_ft
+ * @property string $type
+ * @property int|null $created_at
+ * @property int|null $updated_at
+ * @property-read Collection|PropertyImage[] $images
+ * @property-read int|null $images_count
+ * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static EloquentBuilder|Property newModelQuery()
+ * @method static EloquentBuilder|Property newQuery()
+ * @method static QueryBuilder|Property onlyTrashed()
+ * @method static EloquentBuilder|Property query()
+ * @method static EloquentBuilder|Property whereAddress($value)
+ * @method static EloquentBuilder|Property whereBathrooms($value)
+ * @method static EloquentBuilder|Property whereBedrooms($value)
+ * @method static EloquentBuilder|Property whereCreatedAt($value)
+ * @method static EloquentBuilder|Property whereDescription($value)
+ * @method static EloquentBuilder|Property whereId($value)
+ * @method static EloquentBuilder|Property whereName($value)
+ * @method static EloquentBuilder|Property wherePrice($value)
+ * @method static EloquentBuilder|Property whereSqFt($value)
+ * @method static EloquentBuilder|Property whereType($value)
+ * @method static EloquentBuilder|Property whereUpdatedAt($value)
+ * @method static EloquentBuilder|Property whereUserId($value)
+ * @method static QueryBuilder|Property withTrashed()
+ * @method static QueryBuilder|Property withoutTrashed()
+ * @mixin Eloquent
+ */
 class Property extends Model
 {
     use HasFactory, SoftDeletes, Notifiable, UUID;
-
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
-    protected $table = 'properties';
-
-    /**
-     * The primary key associated with the table.
-     *
-     * @var string
-     */
-    protected $primaryKey = 'id';
 
     /**
      * Indicates if the model's ID is auto-incrementing.
@@ -33,22 +64,30 @@ class Property extends Model
      * @var bool
      */
     public $incrementing = true;
-
-    /**
-     * The data type of the ID.
-     *
-     * @var string
-     */
-    protected $keyType = 'string';
-
     /**
      * Indicates if the model should be timestamped.
      *
      * @var bool
      */
     public $timestamps = false;
-
-
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'properties';
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+    /**
+     * The data type of the ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
     /**
      * The attributes that are mass assignable.
      *
--- app/Models/PropertyImage.php
+++ app/Models/PropertyImage.php
@@ -2,23 +2,35 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
+/**
+ * App\Models\PropertyImage
+ *
+ * @property int $id
+ * @property string $property_id
+ * @property string $path
+ * @property-read Property $property
+ * @method static EloquentBuilder|PropertyImage newModelQuery()
+ * @method static EloquentBuilder|PropertyImage newQuery()
+ * @method static QueryBuilder|PropertyImage onlyTrashed()
+ * @method static EloquentBuilder|PropertyImage query()
+ * @method static EloquentBuilder|PropertyImage whereId($value)
+ * @method static EloquentBuilder|PropertyImage wherePath($value)
+ * @method static EloquentBuilder|PropertyImage wherePropertyId($value)
+ * @method static QueryBuilder|PropertyImage withTrashed()
+ * @method static QueryBuilder|PropertyImage withoutTrashed()
+ * @mixin Eloquent
+ */
 class PropertyImage extends Model
 {
     use HasFactory, SoftDeletes;
-
-    protected $table = 'property_images';
-
-    /**
-     * The primary key associated with the table.
-     *
-     * @var string
-     */
-    protected $primaryKey = 'id';
 
     /**
      * Indicates if the model's ID is auto-incrementing.
@@ -26,15 +38,19 @@ class PropertyImage extends Model
      * @var bool
      */
     public $incrementing = true;
-
     /**
      * Indicates if the model should be timestamped.
      *
      * @var bool
      */
     public $timestamps = false;
-
-
+    protected $table = 'property_images';
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
     /**
      * The attributes that are mass assignable.
      *
