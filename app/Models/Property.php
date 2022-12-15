1057249a9d798e6dfa62703bb8081cddb6698ea4
Zachary Lim <zacharylim2004@gmail.com>
2022-12-15 21:07:16 +0800

    Updated models for property and image tables

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -3,8 +3,6 @@
 namespace App\Models;
 
 use App\Traits\UUID;
-use Eloquent;
-use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -12,57 +10,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Query\Builder as QueryBuilder;
-use Illuminate\Notifications\DatabaseNotification;
-use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
 
-/**
- * App\Models\Property
- *
- * @property string $id
- * @property string $user_id
- * @property string $name
- * @property string $address
- * @property int $bedrooms
- * @property int $bathrooms
- * @property string $description
- * @property string $price
- * @property string|null $sq_ft
- * @property string $type
- * @property int|null $created_at
- * @property int|null $updated_at
- * @property Collection|PropertyImage[] $images
- * @property-read int|null $images_count
- * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
- * @property-read int|null $notifications_count
- * @method static EloquentBuilder|Property newModelQuery()
- * @method static EloquentBuilder|Property newQuery()
- * @method static QueryBuilder|Property onlyTrashed()
- * @method static EloquentBuilder|Property query()
- * @method static EloquentBuilder|Property whereAddress($value)
- * @method static EloquentBuilder|Property whereBathrooms($value)
- * @method static EloquentBuilder|Property whereBedrooms($value)
- * @method static EloquentBuilder|Property whereCreatedAt($value)
- * @method static EloquentBuilder|Property whereDescription($value)
- * @method static EloquentBuilder|Property whereId($value)
- * @method static EloquentBuilder|Property whereName($value)
- * @method static EloquentBuilder|Property wherePrice($value)
- * @method static EloquentBuilder|Property whereSqFt($value)
- * @method static EloquentBuilder|Property whereType($value)
- * @method static EloquentBuilder|Property whereUpdatedAt($value)
- * @method static EloquentBuilder|Property whereUserId($value)
- * @method static QueryBuilder|Property withTrashed()
- * @method static QueryBuilder|Property withoutTrashed()
- * @mixin Eloquent
- * @property int|null $deleted_at
- * @method static EloquentBuilder|Property whereDeletedAt($value)
- */
 class Property extends Model
 {
     use HasFactory, SoftDeletes, Notifiable, UUID;
 
-    public $appends = ['images'];
+    public $appends = ['images', 'rooms', 'address', 'type'];
 
     /**
      * Indicates if the model's ID is auto-incrementing.
@@ -164,6 +118,36 @@ class Property extends Model
     public function images(): HasMany
     {
         return $this->hasMany(PropertyImage::class, 'property_id', 'id');
+    }
+
+    /**
+     * Get the property's type.
+     *
+     * @return Collection
+     */
+    public function getTypeAttribute(): Collection
+    {
+        return $this->type()->get();
+    }
+
+    /**
+     * Get the property's rooms.
+     *
+     * @return Collection
+     */
+    public function getRoomsAttribute(): Collection
+    {
+        return $this->rooms()->get();
+    }
+
+    /**
+     * Get the property's address.
+     *
+     * @return Collection
+     */
+    public function getAddressAttribute(): Collection
+    {
+        return $this->address()->get();
     }
 
     /**
