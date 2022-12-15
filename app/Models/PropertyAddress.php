40df52caa8de082c4d6e5589dc5063be2ed827dc
Zachary Lim <zacharylim2004@gmail.com>
2022-12-15 19:34:43 +0800

    Created models for addresses, rooms and types table

---
+++ app/Models/PropertyAddress.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Notifications\Notifiable;
+
+class PropertyAddress extends Model
+{
+    use HasFactory, Notifiable, UUID;
+
+    /**
+     * The update timestamp associated with the model.
+     *
+     * @var string
+     */
+    const UPDATED_AT = 'updated_date';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'addresses';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'property_id';
+
+    /**
+     * The data type of the ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'line 1',
+        'line 2',
+        'city',
+        'state',
+        'postal_code'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = ['property_id'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = ['updated_at' => 'timestamp'];
+
+    /**
+     * Get the property that owns the rooms.
+     *
+     * @return BelongsTo
+     */
+    public function property(): BelongsTo
+    {
+        return $this->belongsTo(Property::class, 'property_id', 'id');
+    }
+}
