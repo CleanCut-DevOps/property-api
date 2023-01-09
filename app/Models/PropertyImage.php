1ee3d9b1636198b8381058e1e80fdbca92a9eccc
Zachary <zacharylim2004@gmail.com>
2023-01-09 21:48:32 +0800

    Updated migration for image table

--- app/Models/PropertyImage.php
+++ app/Models/PropertyImage.php
@@ -11,12 +11,14 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 /**
  * App\Models\PropertyImage
  *
+ * @property string $public_id
  * @property string $property_id
  * @property string $url
  * @property-read Property $property
  * @method static Builder|PropertyImage newModelQuery()
  * @method static Builder|PropertyImage newQuery()
  * @method static Builder|PropertyImage query()
+ * @method static Builder|PropertyImage wherePublicId($value)
  * @method static Builder|PropertyImage wherePropertyId($value)
  * @method static Builder|PropertyImage whereUrl($value)
  * @mixin Eloquent
@@ -40,18 +42,25 @@ class PropertyImage extends Model
     protected $table = "images";
 
     /**
+     * The data type of the ID.
+     *
+     * @var string
+     */
+    protected $keyType = "string";
+
+    /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = ["property_id"];
+    protected $hidden = ["property_id", "public_id"];
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = ["property_id", "url"];
+    protected $fillable = ["public_id", "property_id", "url"];
 
 
     /**
