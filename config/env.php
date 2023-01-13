53d8cfe29c1bad2e6e2fcf4ce321eaee0a0ae386
Zachary <zacharylim2004@gmail.com>
2023-01-13 10:57:08 +0800

    Updated .env example file

--- config/env.php
+++ config/env.php
@@ -2,7 +2,6 @@
 
 return [
     "ACCOUNT_API" => env("ACCOUNT_API", "https://account-api.klenze.com.au"),
-    "SERVICES_API" => env("SERVICES_API", "https://services-api.klenze.com.au"),
     "PROPERTY_API" => env("PROPERTY_API", "https://property-api.klenze.com.au"),
     "BOOKING_API" => env("BOOKING_API", "https://booking-api.klenze.com.au"),
     "SESSION_API" => env("SESSION_API", "https://session-api.klenze.com.au"),
